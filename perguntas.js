67ab6a9fdfa064b074c4e41bbec893b7382adabb
Isabela <anhaia.isabela@escola.pr.gov.br>
2025-07-06 19:34:06 -0300

    config finais

---
+++ perguntas.js
@@ -0,0 +1,52 @@
+criaCartao(
+    'Historia',
+    'Quem foi o primeiro imperador do Brasil?',
+    'O primeiro imperador foi Dom Pedro I.'
+)
+
+criaCartao(
+    'Historia',
+    'Quem foi o segundo imperador do Brasil?',
+    'O segundo imperador foi Dom Pedro II.'
+)
+
+criaCartao(
+    'Historia',
+    'Quem deu o grito de independencia?',
+    'Foi Dom Pedro II.'
+)
+
+criaCartao(
+    'Geografia',
+    'O que é um país subdesenvolvido?',
+    'É um país com baixo IDH e pouca industrialização.'
+)
+criaCartao(
+    'Geografia',
+    'O que é globalização??',
+    'É a integração econômica, cultural e tecnológica entre países.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é a principal causa do desmatamento na Amazônia?',
+    'Expansão agropecuária.'
+)
+
+criaCartao(
+    'Ingles',
+    'Qual é o passado do verbo "go"?',
+    'Went.'
+)
+
+criaCartao(
+    'Ingles',
+    'What is a synonym for "happy"?',
+    'Glad.'
+)
+
+criaCartao(
+    'Ingles',
+    'Translate: "Ele está estudando agora.',
+    'He is studying now.'
+)
